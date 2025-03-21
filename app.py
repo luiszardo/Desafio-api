7b05e9ac5656b5985db9d3f8beb93ddd3f74e70a
Luis zardo <luisbloomfieldzardo@gmail.com>
2025-03-21 19:36:47 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,73 @@
+import sqlite3
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+
+def init_db():
+    # sqlite3 crie o arquivo database.db
+    with sqlite3.connect("database.db") as conn:
+        conn.execute("""
+                CREATE TABLE IF NOT EXISTS LIVROS(
+                     id INTEGER PRIMARY KEY AUTOINCREMENT,
+                     titulo TEXT NOT NULL,
+                     categoria TEXT NOT NULL,
+                     autor TEXT NOT NULL,
+                     image_url TEXT NOT NULL
+                     )
+        """)
+
+
+init_db()
+
+
+@app.route("/doar", methods=["POST"])
+def doar():
+
+    dados = request.get_json()
+
+    titulo = dados.get("titulo")
+    categoria = dados.get("categoria")
+    autor = dados.get("autor")
+    image_url = dados.get("image_url")
+
+    if not titulo or not categoria or not autor or not image_url:
+        return jsonify({"erro": "Todos os campos são obrigatórios"}), 400
+
+    with sqlite3.connect("database.db") as conn:
+        conn.execute(f"""
+        INSERT INTO LIVROS (titulo, categoria, autor, image_url)
+        VALUES ("{titulo}","{categoria}","{autor}", "{image_url}")
+
+""")
+
+    conn.commit()
+
+    return jsonify({"mensagem": "Livro cadastrado com sucesso"}), 201
+
+
+@app.route("/livros", methods=["GET"])
+def listar_livros():
+    with sqlite3.connect("database.db") as conn:
+        livros = conn.execute("SELECT * FROM LIVROS").fetchall()
+
+        livros_formatados = []
+
+        for item in livros:
+            dicionario_livros = {
+                "id": item[0],
+                "titulo": item[1],
+                "categoria": item[2],
+                "autor": item[3],
+                "image_url": item[4]
+            }
+            livros_formatados.append(dicionario_livros)
+
+    return jsonify(livros_formatados),200
+
+
+if __name__ == "__main__":
+    # Inicia o servidor Flask no modo de depuração
+    # O modo debug faz com que as mudanças no código sejam aplicadas automaticamente, sem necessidade de reiniciar o servidor manualmente
+    app.run(debug=True)
